1f678eb8c72b16fb2a914bb9e9969b158931f252
Ira <mavka.ik@gmail.com>
2021-02-21 18:47:50 +0200

    Add files via upload
    FirstProgramOnFlutter

---
+++ main.dart
@@ -0,0 +1,82 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyFirstApp());
+}
+
+class MyFirstApp extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _MyFirstAppState();
+  }
+}
+class _MyFirstAppState extends State<MyFirstApp> {
+  bool _loading;
+  double _progressValue;
+  @override
+  void initState() {
+    _loading = false;
+    _progressValue = 0.0;
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.lightBlueAccent[200],
+        appBar: AppBar(
+          title: Text("My download simulation"),
+          centerTitle: true,
+        ),
+        body: Center(
+          child: Container(
+            padding: EdgeInsets.all(16),
+            child: _loading  
+            ? Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                LinearProgressIndicator(value: _progressValue),
+                  Text(
+                    '${(_progressValue * 100).round()} %',
+                    style: TextStyle(color: Colors.white, fontSize: 20),
+                  ),
+               ],
+            )
+            : Text(
+                    'Press button to download',
+                    style: TextStyle(color: Colors.white, fontSize: 20),
+                  ),
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            setState(() {
+              _loading = !_loading;
+              _updateProgress();
+            });
+          },
+          child: Icon(Icons.cloud_download),
+        ),
+      ),
+    );
+  }
+  void _updateProgress(){
+    const oneSec = const Duration(seconds: 1);
+    Timer.periodic(oneSec, (Timer t) { 
+      setState(() {
+        _progressValue += 0.2;
+
+        if(_progressValue.toStringAsFixed(1) == '1.0') {
+        _loading = false;
+        t.cancel();
+        _progressValue = 0.0;
+        return;
+        }
+      });
+    }); 
+  }
+}
